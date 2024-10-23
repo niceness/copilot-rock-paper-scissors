37ced8de6609a987ddf13cd599d4e887642cfde2
niceness <niceness@users.noreply.github.com>
2024-10-23 06:15:41 +0000

    rps to rpsls

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import random
 # define main function
 def main():
     # define the game choices
-    choices = ["rock", "paper", "scissors"]
+    choices = ["rock", "paper", "scissors", "lizard", "spock"]
 
     # get the user's choice
-    user_choice = input("Choose rock, paper, or scissors: ")
+    user_choice = input("Choose rock, paper, scissors, lizard, or spock: ")
 
     # get the computer's choice
     computer_choice = random.choice(choices)
@@ -22,11 +22,11 @@ def main():
     # determine the winner
     if user_choice == computer_choice:
         print("It's a tie!")
-    elif user_choice == "rock" and computer_choice == "scissors":
-        print("You win!")
-    elif user_choice == "paper" and computer_choice == "rock":
-        print("You win!")
-    elif user_choice == "scissors" and computer_choice == "paper":
+    elif (user_choice == "rock" and (computer_choice == "scissors" or computer_choice == "lizard")) or \
+         (user_choice == "paper" and (computer_choice == "rock" or computer_choice == "spock")) or \
+         (user_choice == "scissors" and (computer_choice == "paper" or computer_choice == "lizard")) or \
+         (user_choice == "lizard" and (computer_choice == "spock" or computer_choice == "paper")) or \
+         (user_choice == "spock" and (computer_choice == "scissors" or computer_choice == "rock")):
         print("You win!")
     else:
         print("Computer wins!")
