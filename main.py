72eac64e9ca8afcb2ae09799514bf2884e40c1f7
niceness <niceness@users.noreply.github.com>
2024-10-23 06:06:10 +0000

    RPS game

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# Write a Roshambo game
+# TODO: extend to include Lizard and Spock?
+
+import random
+
+# define main function
+def main():
+    # define the game choices
+    choices = ["rock", "paper", "scissors"]
+
+    # get the user's choice
+    user_choice = input("Choose rock, paper, or scissors: ")
+
+    # get the computer's choice
+    computer_choice = random.choice(choices)
+
+    # print the choices
+    print("You chose:", user_choice)
+    print("Computer chose:", computer_choice)
+
+    # determine the winner
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif user_choice == "rock" and computer_choice == "scissors":
+        print("You win!")
+    elif user_choice == "paper" and computer_choice == "rock":
+        print("You win!")
+    elif user_choice == "scissors" and computer_choice == "paper":
+        print("You win!")
+    else:
+        print("Computer wins!")
+
+# call the main function
+if __name__ == "__main__":
+    main()
+exit
